e9b318ac0f56b14fb11792d9f02b7ca0143fe7e0
Ilan Bruski <ilanbruski@gmail.com>
2022-10-25 10:12:34 +0200

    Réponse à la question 13 (création de la méthode particulière string + test) et ajout de tous les commentaires sur la partie "BouteilleBiere.java"

--- TP2_Bieres_BRUSKI/src/tp2_bieres_bruski/BouteilleBiere.java
+++ TP2_Bieres_BRUSKI/src/tp2_bieres_bruski/BouteilleBiere.java
@@ -10,41 +10,51 @@ package tp2_bieres_bruski;
  */
 
 
-
-public class BouteilleBiere {
-    
-    String nom ;
-    double degreAlcool ;
-    String brasserie ;
-    boolean ouverte ;
+public class BouteilleBiere {// création d'une classe nommée "Bouteille bière"
 
     
+    String nom ;// le nom est une chaine
+    double degreAlcool ;// le degrès est un double
+    String brasserie ;// le nom est une chaine
+    boolean ouverte ;// l'état (ouverte/fermée) est un booléen
+
     
-public BouteilleBiere(String p_nom, double p_degreAlcool, String p_brasserie) {
+//     
+public BouteilleBiere(String p_nom, double p_degreAlcool, String p_brasserie) { //création d'un constructeur (il est appelé lors de la création d'objets de type BouteilleBiere)
     nom = p_nom;
     degreAlcool = p_degreAlcool;
     brasserie = p_brasserie;
-    ouverte = false;
+    ouverte = false;//les bieres sont stockées fermées
 }
 
 
 
-public void lireEtiquette() {
+public void lireEtiquette() {// création d'une methode pour lire l'etiquette
     
     System.out.println("Bouteille de " + nom +" (" + degreAlcool +
-    " degres) \nBrasserie : " + brasserie ) ;
+    " degres) \nBrasserie : " + brasserie ) ;// affichage d'une phrase contenant toutes les infos
 }
 
-public boolean Decapsuler() {
-    if (ouverte == false){
-        ouverte = true ;
-        System.out.println("Reussite : biere Ouverte");
-        return true;
+
+public boolean Decapsuler() {// création de la methode pour decapsuler
+    if (ouverte == false){//si la bière est fermée
+        ouverte = true ;//alors on l'ouvre
+        System.out.println("Reussite : biere Ouverte");//puis on affiche que la bière est ouverte
+        return true;//et on renvoie au système en interne le booléen vrai
     }
-    else {
-        System.out.println("Erreur : biere deja ouverte");
-        return false ;
+    else {//sinon (si la bière était fermée)
+        System.out.println("Erreur : biere deja ouverte");//alors on affiche directement que la bière était déjà ouverte
+        return false ;//et on renvoie alors le booléen faux en interne au système
     }
+}
+
+public String ToString () {//Creation d'une methode particulière toString()
+    String chaine_a_retourner;//initalisation de la variable 
+    chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";//on crée une phrase ui demande si une certaine bière est ouverte
+    if (ouverte == true ) chaine_a_retourner += "oui" ; //donc on renvoit la réponse "oui" si la bière est ouverte
+    else chaine_a_retourner += "non" ;// sinon, on renvoit bien entendu "non"
+    return chaine_a_retourner ;//on retourne en interne la réponse
+            
 }
 
 
