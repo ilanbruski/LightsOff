96317dc40a85879c4c9046e1deacccab9f6bf7b3
Ilan Bruski <ilanbruski@gmail.com>
2022-11-08 17:17:37 +0100

    Avancement TP4 question 16

---
+++ TP3_héritage_BRUSKI/src/Armes/Personnage.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author ilanb
+ */
+public class Personnage {
+    String nom;
+    int niveau_de_vie;
+}
