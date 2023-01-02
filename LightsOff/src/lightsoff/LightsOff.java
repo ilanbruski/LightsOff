e5cbabf8e5f11a4a73ae1ad4e94f5011c19b5204
Ilan Bruski <ilanbruski@gmail.com>
2023-01-02 13:42:53 +0100

    je met la variable wincount à 0, définit le tittre et la taille du Jframe, et la disposition du mainPanel, je crée un nouveau panneau, un nouveau tableau et définit la disposition des boutons

--- LightsOff/src/lightsoff/LightsOff.java
+++ LightsOff/src/lightsoff/LightsOff.java
@@ -29,3 +29,20 @@ public class LightsOff extends JFrame implements ActionListener //on crée la cl
 	private JLabel wins;								//un label utilisé pour indiquer à l'utilisateur combien de victoires il a actuellement.
 	private int winCount;								//une variable de comptage pour contenir le nombre actuel de victoires.
         public LightsOff()
+	{
+		winCount = 0;                                                   //définit la variable winCount sur 0.
+		//Termine le programme lorsque l'utilisateur ferme le JFrame.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Définit le titre et la taille du JFrame.
+		setTitle("Lights Out");
+		setSize(500, 500);
+		
+		JPanel mainPanel = new JPanel();				//Le panneau actuel dans lequel tout est placé.
+		
+		mainPanel.setLayout(new BorderLayout());		//définit la disposition du mainPanel.
+		
+		//Boutons
+		JPanel buttonPanel = new JPanel();				//un nouveau panneau pour contenir tous les boutons du plateau de jeu.
+		gameButtons = new JButton[5][5];				//crée un nouveau tableau bidimensionnel qui contiendra 25 boutons.
+		buttonPanel.setLayout(new GridLayout(5,5));		//définit la disposition des boutons.					
